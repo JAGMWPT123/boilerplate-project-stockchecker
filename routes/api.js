14f4a3139de78f6c3bdb6ff1e959fe00a6241043
Marwan <marwan00mr@gmail.com>
2024-01-15 20:59:10 +0200

    working on api

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,28 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+      let stock  = req.query.stock ;
+      let price  = req.query.price ;
+      // let likes  = Number(req.query.likes) ;
       
+      // console.log(price)
+      // console.log(likes)
+      let stockData = {
+        
+        stock : {
+          type : String
+        } , 
+        price : {
+          type : Number
+        } ,
+        likes :{
+          type : Number
+        }  
+      }
+      stockData.stock = stock
+      stockData.price = price
+      console.table(stockData)
+      res.send({stockData})
     });
     
 };
