85a2f7f2368bfb47c53b53522a8f94e8d97f17e0
Marwan Mohammed <53322831+JAGMWPT123@users.noreply.github.com>
2024-01-17 19:10:44 +0000

    faster

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-const helmet      = require("helmet");
-const mongoose    = require('mongoose');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,47 +31,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // mongoose.connection.on("disconnected", () => {
 //   console.log("DB disconnec ted");
 // });
-app.use(helmet({
-    contentSecurityPolicy: {    // enable and configure
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      // enable and configure
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"]
-      }
+        styleSrc: ["'self'", "'unsafe-inline'"],
+      },
     },
-  }))
+  }),
+);
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-    res.status(200)
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+  res.status(200);
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+//
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
   //connect();
-  if(process.env.NODE_ENV==='test') {
+  const NODE_ENV = "test";
+  if (NODE_ENV === "test") {
     setTimeout(function () {
-      console.log('[+] Running Tests...');
+      console.log("[+] Running Tests...");
       try {
         runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
+      } catch (e) {
+        console.log("Tests are not valid:");
         console.error(e);
       }
     }, 3500);
